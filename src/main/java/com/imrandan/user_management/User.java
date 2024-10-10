379849db7bbee70421cf6248f1037020bb6ed4cf
Danyal Imran <dan.imran97@gmail.com>
2024-10-10 13:28:12 -0400

    set getters and setters and user constructor

--- src/main/java/com/imrandan/user_management/User.java
+++ src/main/java/com/imrandan/user_management/User.java
@@ -14,6 +14,47 @@ public class User {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.phoneNumber = phoneNumber
+        this.phoneNumber = phoneNumber;
+    }
+
+    //The Getters and The Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+ 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
