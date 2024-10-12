00189fea9f323d297d857d41cf2eaf03deff77d2
Danyal Imran <dan.imran97@gmail.com>
2024-10-12 07:47:33 -0400

    renamed User entity to a table called users so there is no conflict with reserved sql keywords

--- src/main/java/com/imrandan/user_management/model/User.java
+++ src/main/java/com/imrandan/user_management/model/User.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users") // Changed the table name to "users"
 public class User {
 
     @Id
