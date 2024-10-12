2ed459dd2167361d5e3ed0df8489b9c6a77fb2ef
Danyal Imran <dan.imran97@gmail.com>
2024-10-12 08:14:09 -0400

    updated application properties to have peristance storage and usercontroller to allow getting a user by their id

--- src/main/java/com/imrandan/user_management/controller/UserController.java
+++ src/main/java/com/imrandan/user_management/controller/UserController.java
@@ -3,6 +3,8 @@ package com.imrandan.user_management.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,8 +42,14 @@ public class UserController {
     
     // Get a user by their ID
     @GetMapping("/users/{id}") 
-    public Optional<User> getUserById(@PathVariable Long id) {
-        return userRepository.findById(id);
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+
+        if (optionalUser.isPresent()) {
+            return ResponseEntity.ok(optionalUser.get()); //this will return the user if found
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null); // else return a 404
+        }
     }
 
     // Updates user
